998a2c2230a2ec60a26e609d2d7cc9bcf043b893
Zahid046 <zmemon046@gmail.com>
2022-08-28 17:06:26 +0600

    image and text view implementation

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View,Image,SafeAreaView } from 'react-native';
 
 export default function App() {
+ const handlePress=()=>console.log("hello");
+//  console.log(require('./assets/icon.png'));
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>React Native App</Text>
+      <Image 
+      fadeDuration={1500}
+      style={styles.imageStyle}
+      source={
+        require('./assets/adaptive-icon.png')
+        }
+      />
+      <Image 
+      blurRadius={2}
+      fadeDuration={1000}
+      source={{
+        width : 100,
+        height:150,
+        uri:"https://picsum.photos/200",
+      }}
+      />
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageStyle: {
+    backgroundColor:'white',
+    width: 100,
+    height: 150,
   },
 });
